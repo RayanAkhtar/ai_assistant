c2c9ff7bc87cca4cd606691a74f45ca04645b22d
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2024-09-03 16:11:47 +0100

    chore: more stubs and slightly altered plan

--- src/AIAssistant.py
+++ src/AIAssistant.py
@@ -1,20 +1,29 @@
 
+def cleanup():
+    """Removing files in tmp and rec"""
+    pass
+
 
 def record_audio():
+    """Records audio from the speaker (for now)"""
     pass
 
 def transcribe_audio():
+    """Transcribes the audio file that was just recorded"""
     pass
 
 def pass_prompt():
+    """Runs the Prompt on an LLM to get a relevant output"""
     pass
 
 def display_message():
+    """Displays suggestions for the user, based off the LLM's output"""
     pass
 
 
 
 if __name__ == "__main__":
+    cleanup()
     record_audio()
     transcribe_audio()
     pass_prompt()

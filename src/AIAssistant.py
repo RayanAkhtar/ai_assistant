c86d770b820a7d38f03d056fdcc08804521abddd
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2024-09-03 16:01:42 +0100

    feat: implement plan with stubs to fill in for ai assistant

---
+++ src/AIAssistant.py
@@ -0,0 +1,21 @@
+
+
+def record_audio():
+    pass
+
+def transcribe_audio():
+    pass
+
+def pass_prompt():
+    pass
+
+def display_message():
+    pass
+
+
+
+if __name__ == "__main__":
+    record_audio()
+    transcribe_audio()
+    pass_prompt()
+    display_message()
